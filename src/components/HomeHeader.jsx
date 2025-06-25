0e9f2e23b7747b10e48e9a4621177bbde3093090
Joanna <40646853+Joabulla07@users.noreply.github.com>
2025-06-25 11:51:44 -0300

    remove unnecesary lines

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logoGestionar.png";
 import Header from "./Header.jsx";
 
-// export default function HomeHeader() {
-//     return (
-//         <nav>
-//             <ul className="navbar">
-//                 <li className="left-logo">
-//                     <img className="logo" src={logo} alt="logo" />
-//                 </li>
-//                 <li className="left-logo">
-//                     <h2 className="h2">GestionAR</h2>
-//                 </li>
-//                 <li>
-//                     <a href="#contacto">Contacto</a>
-//                 </li>
-//                 <li>
-//                     <a href="#noticias">Noticias</a>
-//                 </li>
-//                 <li>
-//                     <a href="#ventajas">Ventajas</a>
-//                 </li>
-//                 <li>
-//                     <Link to="/login">Ingresar/Registrarse</Link>
-//                 </li>
-//             </ul>
-//         </nav>
-//     );
-// }
 
 export default function HomeHeader() {
     return (
