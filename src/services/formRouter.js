e7011be447cfa08ece6c12748548dafe59e242e6
Joanna <40646853+Joabulla07@users.noreply.github.com>
2025-09-08 17:04:41 -0300

    logs

--- src/services/formRouter.js
+++ src/services/formRouter.js
@@ -2,6 +2,8 @@
 
 export const formRouter = async (payload) => {
     const API_URL = import.meta.env.VITE_API_URL; // definido en .env
+    console.log("por enviar mail...")
+    console.log(payload)
     try  {
         const res = await fetch(`${API_URL}/api/email/send-to-me`, {
             method: "POST",
@@ -10,6 +12,8 @@ export const formRouter = async (payload) => {
             },
             body: JSON.stringify(payload)
         });
+
+        console.log(res.json().then(data => console.log(data)))
 
         if (!res.ok) {
             const text = await res.text().catch(() => "");
